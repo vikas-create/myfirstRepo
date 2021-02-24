79b1a04ed13f8798674475fe72d26bb430a00779
vikas-create <77847642+vikas-create@users.noreply.github.com>
2021-02-25 00:16:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_instance" "test" {
 
   }
   provisioner "local-exec" {
-    command = "ssh-copy-id -i /root/.ssh/id_rsa.pub ec2-user@${aws_instance.test.public_ip}"
+    command = "ssh-copy-id -i /home/ec2-user/.ssh/id_rsa.pub ec2-user@${aws_instance.test.public_ip}"
   }
   provisioner "local-exec" {
     command = "ansible-playbook -i ${aws_instance.test.public_ip}, --private-key ${local.private_key_path} /etc/ansible/httpd.yml"
