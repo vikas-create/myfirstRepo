59d49c6f1bc54941b9730a0b9eb3079585f122ea
Vikas Singh <Vikas.Singh@test.com>
2021-02-24 22:46:40 +0530

    a

--- main.tf
+++ main.tf
@@ -8,9 +8,41 @@ provider "aws" {
   region = "us-east-2"
 }
 
+resource "aws_security_group" "httpd-sg" {
+  name = "httpd-access"
+  vpc_id = "vpc-61a8630a"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "test"
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+  }
+
+  ingress {
+    
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "test"
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80  
+  }
+
+  egress = {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "test"
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+  } 
+  
+}
+
 resource "aws_instance" "test" {
   ami           = "ami-09246ddb00c7c4fef"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.httpd-sg.id]
   key_name      = local.key_name
   tags = {
     "Name" = "tf-instance"
